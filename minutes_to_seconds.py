b2dd3cfb5c7f360ed6c03c7759231e7544212e2a
Cesar Medrano <cesarmedrano5@gmail.com>
2021-03-08 22:55:50 -0600

    minutes to seconds fixed to print with function result in a line

--- minutes_to_seconds.py
+++ minutes_to_seconds.py
@@ -5,12 +5,4 @@ def minutestoseconds(minutes, secondsinaminute):
     result = minutes*secondsinaminute
     return result
 
-print("There are")
-
-print(minutestoseconds(minutes,secondsinaminute))
-print("seconds")
-print("in")
-print(minutes)
-print("minutes")
-
-print("There are:" minutestoseconds(minutes,secondsinaminute) "seconds")
+print(f'there are {minutestoseconds(minutes,secondsinaminute)} seconds in {minutes}')
